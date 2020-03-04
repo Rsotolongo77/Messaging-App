21a4b5d216145bc373eedc434c8a1a26f88bfeb9
Ricardo Sotolongo <rickey.soto@gmail.com>
2020-03-04 06:38:43 -0500

    added first useEffect to chat component.

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const router = require("./router");
 
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on("connection", (socket) => {
+io.on("connect", (socket) => {
     console.log("We have a new connection.");
 
     socket.on("disconnect", () => {
