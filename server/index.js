97bc0a91fddd76acf635137e0eff28a9ae1cef1b
Ricardo Sotolongo <rickey.soto@gmail.com>
2020-03-09 08:15:22 -0400

    added array to hold messages on client side with corresponding useEffect.

--- server/index.js
+++ server/index.js
@@ -30,6 +30,10 @@ io.on("connect", (socket) => {
 
     socket.on("sendmessage", (message, callback) => {
         const user = getUser(socket.id);
+
+        io.to(user.room).emit("message", { user: user.name, text: message });
+
+        callback();
     });
 
     socket.on("disconnect", () => {
