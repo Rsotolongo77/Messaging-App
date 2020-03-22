7d7895e5e15367237ca328272c2b2d7e2e6212ab
Ricardo Sotolongo <rickey.soto@gmail.com>
2020-03-22 10:41:05 -0400

    debugging Heroku deploy.

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
     const [users, setUsers] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = 'https://git.heroku.com/quiet-cliffs-01083.git';
+    const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
